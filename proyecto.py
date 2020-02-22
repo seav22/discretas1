bbf9f0e5b47fa8a9b56d26e777e4a02e7c62ddbf
Sebastian <seav22@gmail.com>
2020-02-22 19:29:21 -0400

    agregue boton y text area

--- proyecto.py
+++ proyecto.py
@@ -6,8 +6,11 @@ except ImportError:
 
 from tkinter import *
 
+def accionBoton():
+    print("Botoncito dandola")
+
 class UI(tk.Frame):
-    """Docstring."""
+    #Docstring
 
     def __init__(self, parent=None):
         tk.Frame.__init__(self, parent)
@@ -15,15 +18,24 @@ class UI(tk.Frame):
         self.init_ui()
 
     def init_ui(self):
-        """Aqui colocariamos los widgets."""
-        self.parent.title("Un titulo para la ventana")
+        #Widgets de la UI
+        self.parent.title("Estructuras Algebraicas")
 
+        #widget label
         etiqueta = tk.Label(self.parent, text="Ejemplo simple de Label")
         etiqueta.pack()
+        #widget boton
+        boton = tk.Button(ROOT, text="esto es un boton", command=accionBoton)
+        boton.pack()
+        #widget text area
+        textArea = tk.Entry(ROOT)
+        textArea.pack()
+        textArea.insert(0, "marselo")
+        textArea.get()
 
 if __name__ == "__main__":
     ROOT = tk.Tk()
-    ROOT.geometry("300x100")
+    ROOT.geometry("800x600")
     APP = UI(parent=ROOT)
     APP.mainloop()
     ROOT.destroy()
