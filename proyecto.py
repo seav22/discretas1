f3fddc59b185b22ba02a09357c2cfdaf752beda3
Sebastian <seav22@gmail.com>
2020-02-22 11:56:32 -0400

    agregue checkeo de libreria al inicio y titulo a la ventana

--- proyecto.py
+++ proyecto.py
@@ -1,7 +1,15 @@
-import tkinter
+#checkeo de libreria necesaria
+try:
+    import tkinter as tk
+except ImportError:
+    raise ImportError("Se requiere la libreria tkinter para continuar")
+
 from tkinter import *
 
-root = tkinter.Tk()
+#configuracion de la pantalla
+root = tk.Tk()
 root.config(width=800, height=600)
+root.title("Estructuras Algebraicas")
+root.mainloop()
 
 
